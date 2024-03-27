066788d324903f36c47bf38466b99ab822060d6c
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2024-03-27 16:44:14 +0100

    basic streaming for client components and server components

--- examples/next-js-example/src/app/page.tsx
+++ examples/next-js-example/src/app/page.tsx
@@ -1,12 +1,40 @@
 import Image from "next/image";
+import Client from "@/components/client";
+import { Ore } from "@glamboyosa/ore";
 
+const ore = new Ore({
+  url: "http://localhost:4000/server-component/",
+  headers: {
+    "Cache-Control": "no-cache",
+  },
+});
+let chat = "";
+ore.fetchSSE(
+  (buffer, parts) => {
+    console.log("Received buffer:", buffer);
+    chat += buffer;
+    // Process the received buffer
+
+    // const b = parts[parts.length - 1];
+    // console.log("Received parts i.e. events", parts);
+    // console.log("Buffer per event for server component", b);
+  },
+  (streamEnded) => {
+    console.log("Stream ended", streamEnded);
+  }
+);
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+        <p className="fixed left-0 top-0 flex w-full flex-col justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          <div className="flex p-4 justify-center items-center gap-4">
+            <h4>Server Component stream:</h4>
+            <p className="w-1/2">{chat}</p>
+          </div>
+          <div className="flex p-4 justify-center items-center gap-4">
+            <Client />
+          </div>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -37,76 +65,6 @@ export default function Home() {
           height={37}
           priority
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
---
+++ examples/next-js-example/src/components/client.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useEffect, useState, useRef } from "react";
+import { Ore } from "@glamboyosa/ore";
+type ClientProps = {};
+const ore = new Ore({
+  url: "http://localhost:4000/",
+  headers: {
+    "Cache-Control": "no-cache",
+  },
+});
+
+const Client: React.FC<ClientProps> = () => {
+  const [chat, setCurrentChat] = useState("");
+  const isFirstRun = useRef(true);
+  useEffect(() => {
+    if (isFirstRun.current) {
+      isFirstRun.current = false;
+      console.log("console:");
+      ore.fetchSSE(
+        (buffer, parts) => {
+          //  console.log("Received buffer:", buffer);
+          setCurrentChat(buffer);
+          // Process the received buffer
+
+          const b = parts[parts.length - 1];
+          //console.log("Received parts i.e. events", parts);
+          // console.log("Buffer per event", b);
+        },
+        (streamEnded) => {
+          console.log("Stream ended", streamEnded);
+        }
+      );
+    }
+  }, []);
+  return (
+    <div className="flex w-full items-center justify-center gap-4">
+      <h4> Client Component:</h4>
+      <p className="w-1/2">{chat}</p>
+    </div>
+  );
+};
+
+export default Client;
