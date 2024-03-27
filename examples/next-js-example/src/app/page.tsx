5537e88cb1f3774d67e144bc6a503ed2e2ccd9e1
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2024-03-27 16:47:14 +0100

    better example

--- examples/next-js-example/src/app/page.tsx
+++ examples/next-js-example/src/app/page.tsx
@@ -9,21 +9,21 @@ const ore = new Ore({
   },
 });
 let chat = "";
-ore.fetchSSE(
-  (buffer, parts) => {
-    console.log("Received buffer:", buffer);
-    chat += buffer;
-    // Process the received buffer
-
-    // const b = parts[parts.length - 1];
-    // console.log("Received parts i.e. events", parts);
-    // console.log("Buffer per event for server component", b);
-  },
-  (streamEnded) => {
-    console.log("Stream ended", streamEnded);
-  }
-);
 export default function Home() {
+  ore.fetchSSE(
+    (buffer, parts) => {
+      console.log("Received buffer:", buffer);
+      chat += buffer;
+      // Process the received buffer
+
+      // const b = parts[parts.length - 1];
+      // console.log("Received parts i.e. events", parts);
+      // console.log("Buffer per event for server component", b);
+    },
+    (streamEnded) => {
+      console.log("Stream ended", streamEnded);
+    }
+  );
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
